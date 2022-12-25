2500ee0f5236ba6b5f45a5ee137b792d4de7a927
Denis <denisgreshnaykov@gmail.com>
2022-12-25 23:02:35 +0500

    add popup cookie cookieStorage

--- js/cookieConsent.js
+++ js/cookieConsent.js
@@ -1,5 +1,18 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const storageType = localStorage;
+  const cookieStorage = {
+    getItem: (key) => {
+      const cookies = document.cookie
+        .split(";")
+        .map((cookie) => cookie.split("="))
+        .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+      return cookies[key];
+    },
+    setItem: (key, value) => {
+      document.cookie = `${key}=${value};expires=Sun, 16 Jul 3567 06:23:41 GMT`;
+    },
+  };
+
+  const storageType = cookieStorage;
   const consentPropertyType = "site_consent";
 
   //следует ли показывать окно с куки
